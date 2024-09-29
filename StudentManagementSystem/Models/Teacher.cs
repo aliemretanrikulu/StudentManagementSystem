b56f380171a67f20801843e6ba3ac8458a42d974
aliemretanrikulu <aliemretanrikulu@gmail.com>
2024-09-30 02:00:33 +0300

    metodlar eklendi ve constructor oluşturuldu

---
+++ StudentManagementSystem/Models/Teacher.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentManagementSystem.Models;
+
+public class Teacher
+{
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"Öğretmen adı: {Name}, Öğretmen Soyadı: {Surname}, Öğretmen yaşı: {Age}";
+    }
+}
--- StudentManagementSystem/Repository/StudentRepository.cs
+++ StudentManagementSystem/Repository/StudentRepository.cs
@@ -4,7 +4,7 @@ namespace StudentManagementSystem.Repository;
 
 public class StudentRepository : Entity
 {
-    List<Student> students = new List<Student>()
+    private List<Student> students = new List<Student>()
     {
         new Student(1, "Emre", "Tanrıkulu", 25, "Halkalı"),
         new Student(2, "İlayda", "Kudun", 22, "Halkalı"),
@@ -14,4 +14,36 @@ public class StudentRepository : Entity
         new Student(6, "Şakir", "Tekin", 25, "Sefaköy"),
     };
 
+    public List<Student> GetStudents()
+    {
+        return students;
+    }
+
+    public void AddStudent(Student student)
+    {
+        students.Add(student);
+    }
+
+    public void RemoveStudent(int id)
+    {
+        var student = students.FirstOrDefault(s => s.Id == id);
+        if (student is null)
+        {
+            students.Remove(student);
+
+        }
+    }
+
+    public void UpdateStudent(int id, Student updated)
+    {
+        var student = students.FirstOrDefault(s => s.Id == id);
+        if (student is null)
+        {
+            student.Id = id;
+            student.Name = updated.Name;
+            student.Surname = updated.Surname;
+            student.Age = updated.Age;
+            student.Location = updated.Location;
+        }
+    }
 }
